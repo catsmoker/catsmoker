2096cf466b099aa78335cc071dc8fd6b3759cb68
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-03-14 16:14:58 +0000

    Create .hello.js

---
+++ .hello.js
@@ -0,0 +1,4 @@
+// This script prints a greeting message to the console.
+
+const greeting = "hello"; // Define the greeting message
+console.log(`${greeting} world!`); // Log the greeting message followed by "world" to the console
