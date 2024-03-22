5d6227d02b2daae95659b8962ee6183eca2b2c36
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-03-22 00:10:04 +0000

    Delete .main.js

--- .main.js
+++
@@ -1,4 +0,0 @@
-// This script logs a message indicating the presence of a "catsmoker" to the console.
-
-const creature = "catsmoker"; // Define the creature as "catsmoker"
-console.log(`Beware! A ${creature} is nearby!`); // Log a warning about the presence of the creature
